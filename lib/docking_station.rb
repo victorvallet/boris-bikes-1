40e850af88bb3b39fbe217e9907d65a67300e8b5
Benjamin Tomkins <bentomkins@hotmail.com>
2018-05-17 09:09:06 +0100

    step 15

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,20 +2,31 @@ require_relative 'bike'
 
 class DockingStation
 
-  attr_reader :bike
+  attr_accessor :bike
 
   def initialize
     @bikes = []
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bikes.count >= 20
+    fail 'Docking station full' if full?
     @bikes << bike
+  end
+
+
+  private
+  def full?
+  	@bikes.count >= 20 ? true : false
+  end
+
+  private
+  def empty?
+  	@bikes.empty?
   end
 
 end
