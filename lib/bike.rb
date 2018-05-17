9c119c2a5e874c456151793350ff5fded8b1879c
Benjamin Tomkins <bentomkins@hotmail.com>
2018-05-17 16:38:46 +0100

    step 18a

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,33 @@
 class Bike
+
+  attr_reader 
+
+  def report_broken
+  	@broken = true
+  end
+
+  def broken?
+  	@broken
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # ignore me
   def working?
     true
   end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,10 +1,18 @@
 require 'bike'
 
 describe Bike do
-  it { is_expected.to respond_to :working? }
 
-  it 'get a bike' do
-    expect(subject).to be_working
-  end
+	it 'if bike is broken' do
+		is_expected.to respond_to :working? 
+  	end
+
+  	it 'the bike can be reported as broken, so do that maybe??' do
+  		subject.report_broken
+  		expect(subject).to be_broken
+  	end
+
+  # it 'get a bike' do
+  #   expect(subject).to be_working
+  # end
 
 end
